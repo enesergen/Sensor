8caea9c8894d9c68fc768ce0efe520686515686b
enes <eenes44@gmail.com>
2023-08-04 15:46:03 +0300

    .

--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -1,12 +1,19 @@
 import org.apache.commons.lang3.SerializationUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
-import java.lang.annotation.Target;
 import java.math.RoundingMode;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.DecimalFormat;
-import java.util.concurrent.Semaphore;
 
 public class Sensor implements Serializable {
 
@@ -16,9 +23,19 @@ public class Sensor implements Serializable {
     private double xVelocity;
     private double yVelocity;
     private static final DecimalFormat decimalFormat = new DecimalFormat("0.00");
+    private static int sensorServerPort;
+    private static int processServerPort;
+    private static String processServerAddress;
 
-    private Semaphore semaphore1;
-    private Semaphore semaphore2;
+    private String message;
+
+    public synchronized String getMessage() {
+        return message;
+    }
+
+    public synchronized void setMessage(String message) {
+        this.message = message;
+    }
 
     public Sensor(String sensorName, double x, double y, double xVelocity, double yVelocity) {
         this.sensorName = sensorName;
@@ -27,10 +44,8 @@ public class Sensor implements Serializable {
         this.xVelocity = xVelocity;
         this.yVelocity = yVelocity;
         decimalFormat.setRoundingMode(RoundingMode.UP);
-        semaphore1 = new Semaphore(1);
-        semaphore2 = new Semaphore(0);
+        message = "";
     }
-
 
     public void createServer(int port) {
         try (ServerSocket server = new ServerSocket(port)) {
@@ -47,7 +62,75 @@ public class Sensor implements Serializable {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    private void sendProcessServer(String address, int portNumber) {
+        Socket socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        boolean isConnected = provideFirstConnection(address, portNumber, socket, oos, ois);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        while (true) {
+
+            if (isConnected) {
+                try {
+                    assert oos != null;
+                    oos.writeObject(getMessage());
+                    oos.flush();
+                    String msg = (String) ois.readObject();
+                    System.out.println("Process Server Response:" + msg);
+                } catch (IOException | ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+            } else {
+                try {
+                    socket = new Socket(address, portNumber);
+                    oos = new ObjectOutputStream(socket.getOutputStream());
+                    ois = new ObjectInputStream(socket.getInputStream());
+                    isConnected = true;
+                    Thread.sleep(400);
+                } catch (IOException | InterruptedException e) {
+                    isConnected = false;
+                    System.out.println("Connection failed. It will try to provide connection");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public String sensorToString() {
+        return sensorName + "," + x + "," + y + "," + xVelocity + "," + yVelocity + ",";
+    }
+
+    public boolean provideFirstConnection(String address, int portNumber, Socket socket, ObjectOutputStream oos, ObjectInputStream ois) {
+        boolean isConnected = false;
+        while (!isConnected) {
+            try {
+                socket = new Socket(address, portNumber);
+                oos = new ObjectOutputStream(socket.getOutputStream());
+                ois = new ObjectInputStream(socket.getInputStream());
+                isConnected = true;
+                System.out.println("Connection is provided for port:" + portNumber);
+            } catch (IOException e) {
+                System.out.println("Try to connect sensor");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+        return isConnected;
     }
 
     private static class TargetHandler implements Runnable {
@@ -62,24 +145,82 @@ public class Sensor implements Serializable {
         @Override
         public void run() {
 
-                try (
-                        ObjectInputStream ois = new ObjectInputStream(target.getInputStream());
-                        ObjectOutputStream oos = new ObjectOutputStream(target.getOutputStream())
-                ) {
-                    while (true){
-                        String data=(String)ois.readObject();
-                        oos.writeObject("Data alındı");
-                        System.out.println(data);
-                    }
-
-                } catch (IOException | ClassNotFoundException e) {
-                    System.out.println("Target connection is broken");
+            try (
+                    ObjectInputStream ois = new ObjectInputStream(target.getInputStream());
+                    ObjectOutputStream oos = new ObjectOutputStream(target.getOutputStream())
+            ) {
+                while (true) {
+                    String data = (String) ois.readObject();
+                    sensor.setMessage(sensor.sensorToString() + data);
+                    oos.writeObject(sensor.sensorName + ":Data received");
+                    System.out.println(data);
                 }
+
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Target connection is broken");
+            }
 
 
         }
     }
 
+    public void updateCoordinates() {
+        while (true) {
+            synchronized (this) {
+                if (x > 500 | x < -500) {
+                    xVelocity *= -1;
+                }
+                if (y > 500 | y < -500) {
+                    yVelocity *= -1;
+                }
+                this.x += xVelocity;
+                this.y -= yVelocity;
+                this.x = formatAndRoundNumber(x);
+                this.y = formatAndRoundNumber(y);
+                System.out.println("Update Func " + "X:" + x + " Y:" + y);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public double formatAndRoundNumber(double number) {
+        return Double.parseDouble(decimalFormat.format(number));
+    }
+
+    public static Sensor readObjectFromXML(String xmlPath) {
+        Sensor sensor = null;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new File(xmlPath));
+            doc.getDocumentElement().normalize();
+            NodeList list = doc.getElementsByTagName("Sensor");
+
+            for (int temp = 0; temp < list.getLength(); temp++) {
+                Node node = list.item(temp);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    sensor = new Sensor(element.getElementsByTagName("sensorName").item(0).getTextContent(),
+                            Double.parseDouble(element.getElementsByTagName("x").item(0).getTextContent()),
+                            Double.parseDouble(element.getElementsByTagName("y").item(0).getTextContent()),
+                            Double.parseDouble(element.getElementsByTagName("xVelocity").item(0).getTextContent()),
+                            Double.parseDouble(element.getElementsByTagName("yVelocity").item(0).getTextContent()));
+                    sensorServerPort = Integer.parseInt(element.getElementsByTagName("sensorServerPort").item(0).getTextContent());
+                    processServerPort = Integer.parseInt(element.getElementsByTagName("processServerPort").item(0).getTextContent());
+                    processServerAddress = element.getElementsByTagName("processServerAddress").item(0).getTextContent();
+                }
+            }
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+        return sensor;
+    }
 
     public double getX() {
         return x;
@@ -113,21 +254,6 @@ public class Sensor implements Serializable {
         this.yVelocity = yVelocity;
     }
 
-    public Semaphore getSemaphore1() {
-        return semaphore1;
-    }
-
-    public void setSemaphore1(Semaphore semaphore1) {
-        this.semaphore1 = semaphore1;
-    }
-
-    public Semaphore getSemaphore2() {
-        return semaphore2;
-    }
-
-    public void setSemaphore2(Semaphore semaphore2) {
-        this.semaphore2 = semaphore2;
-    }
 
     public String getSensorName() {
         return sensorName;
@@ -138,9 +264,15 @@ public class Sensor implements Serializable {
     }
 
     public static void main(String[] args) {
-        Sensor sensor = new Sensor("Sensor-1", 200, 300, 0, 0);
-        sensor.createServer(8080);
-
+        Sensor sensor = readObjectFromXML("C:\\Users\\stj.eergen\\Desktop\\Sensor\\src\\main\\resources\\sensor1.xml");
+        Thread t1 = new Thread(sensor::updateCoordinates);
+        t1.start();
+        new Thread(() -> {
+            sensor.createServer(sensorServerPort);
+        }).start();
+        new Thread(() -> {
+            sensor.sendProcessServer(processServerAddress, processServerPort);
+        }).start();
     }
 }
 
